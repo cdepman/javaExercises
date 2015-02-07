b9f93c3419c61e1a70c96b86cbeca38c597143db
Charlie Depman <cdepman@gmail.com>
2015-02-07 10:52:19 -0800

    updates StringReverse

--- StringReverse.java
+++ StringReverse.java
@@ -2,12 +2,11 @@ class StringReverse{
 	public static void main(String[] args){
 		char[] chars = args[0].toCharArray();
 		char[] result = new char[chars.length];
-		System.out.println("Chars Length: " + chars.length);
-		System.out.println("Result Length: " + result.length);
 		for (int i = chars.length - 1; i >= 0; i = i - 1) {
 
 			result[i] = chars[chars.length - i - 1];
 		}
-		System.out.println(result);
+		String reversedString = new String(result);
+		System.out.println(reversedString);
 	}
 }
