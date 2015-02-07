c4d02b58669e07f35fd7dcebac7f262cfe5a2e04
Charlie Depman <cdepman@gmail.com>
2015-02-07 11:11:03 -0800

    refactors StringReverse

--- StringReverse.java
+++ StringReverse.java
@@ -1,12 +1,17 @@
 class StringReverse{
-	public static void main(String[] args){
-		char[] chars = args[0].toCharArray();
+	static String StringReverse(String stringToReverse){
+		char[] chars = stringToReverse.toCharArray();
 		char[] result = new char[chars.length];
 		for (int i = chars.length - 1; i >= 0; i = i - 1) {
 
 			result[i] = chars[chars.length - i - 1];
 		}
 		String reversedString = new String(result);
-		System.out.println(reversedString);
+		return reversedString;
+	}
+
+	public static void main(String[] args){
+		StringReverse reverse = new StringReverse();
+		System.out.println(reverse.StringReverse(args[0]));
 	}
 }
