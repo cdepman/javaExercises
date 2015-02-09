c34e901018ecc2e3c27ec017f7beafcdf6051a76
Charlie Depman <cdepman@gmail.com>
2015-02-08 19:45:57 -0800

    Stack initial commit

---
+++ Stack.java
@@ -0,0 +1,36 @@
+class Stack{
+
+  private int[] storage = new int[10];
+  private int count = 0;
+
+  public int pop(){
+    int temp = storage[count-1];
+    count = count - 1;
+    for (int i = 0; i < storage.length-1; i = i + 1){
+      storage[i] = storage[i+1];
+    }
+    return temp;
+  }
+
+  public void push(int value){
+    storage[count] = value;
+    count = count + 1;
+  }
+
+  public int length(){
+    return count;
+  }
+
+  public static void main(String[] args){
+    Stack myStack = new Stack();
+    myStack.push(1);
+    myStack.push(3);
+    myStack.push(5);
+    System.out.println(myStack.length());
+    System.out.println(myStack.pop());
+    System.out.println(myStack.length());
+    myStack.push(88);
+    System.out.println(myStack.length());
+    myStack.push(99);
+  }
+}
