b17699c42c4c74d121226c6582dff9f250bb5939
Charlie Depman <cdepman@gmail.com>
2015-02-11 23:10:41 -0800

    adds RockPaperScissors, incomplete

---
+++ RockPaperScissors.java
@@ -0,0 +1,22 @@
+class RockPaperScissors{
+
+  String[] plays = {"Rock", "Paper", "Scissors"};
+  String[][] result = String[27][];
+  int rounds = 3;
+
+  public RPSCombinations(String[] playedSoFar){
+    if (playedSoFar.length == rounds){
+      result[result.length] = playedSoFar;
+    } else {
+      
+      RPSCombinations()
+    }
+  }
+
+  public 
+
+
+  public static void main(String[] args){
+
+  }
+}
