dc2c01a943884415d1ed819f8fca32badef2a446
Charlie Depman <cdepman@gmail.com>
2015-02-13 09:58:32 -0800

    adds ArrayExercises

---
+++ ArrayExercises.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Arrays;
+
+public class ArrayExercises{
+  public static void main(String[] args){
+    ArrayList<String> list = new ArrayList<String>();
+
+    list.add("Remus");
+    list.add("Romulus");
+    list.add("Rhea Silvia");
+    list.add("Mars");
+
+    for (String s: list){
+      System.out.println(s);
+    }
+
+    for (Iterator<String> iter = list.iterator(); iter.hasNext();) {
+      String nextItem = iter.next();
+      if (nextItem.equals("Mars")){
+        iter.remove();
+      } else {
+        System.out.println(nextItem);
+      }
+    }
+
+    for (String s: list){
+      System.out.println(s);
+    }
+  }
+}
---
+++ DayTester.java
@@ -0,0 +1,8 @@
+public class DayTester{
+
+  public static void main(String[] args) {
+    Day dayTest = new Day( 1964, 11, 28);
+    dayTest.addDays( 228 );
+    System.out.println( dayTest );
+  }
+}
